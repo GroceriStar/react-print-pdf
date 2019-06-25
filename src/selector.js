58824fa31551034df0c00e71bc8b37d552a00ef5
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-25 10:50:35 +0300

    lodash is fixed?

--- src/selector.js
+++ src/selector.js
@@ -1,39 +1,22 @@
 
 import chickenKyiv from '@groceristar/recipes-methods'
 import groceristar from '@groceristar/fetch-grocery-list'
-// import
-  // groceristar
-
- // from "@groceristar/groceristar-fetch/groceristar";
-
-// import
-
-  // chickenKyiv
- // from "@groceristar/groceristar-fetch/chickenKyiv";
-
-// import eee from "@groceristar/groceristar-fetch";
-// console.log(eee);
-// console.log(groceristar);
 
 function getGroceryById( id ) {
   return groceristar.getGroceryById(id);
-  // return ""
 }
 
 function getFullGrocery( name ) {
   return groceristar.getGroceryByNameWithDepAndIngKey(name);
   // return groceristar.getGroceryByNameWithDepAndIng(name);
-  // return ""
 }
 
 function getGroceryCollection(){
   return groceristar.getGroceryShowcase();
-  // return ""
 }
 
 function getAllGrocery(){
   return groceristar.getAllGrocery();
-  // return ""
 }
 
 // @TODO move out to fetch plugin?
@@ -48,7 +31,6 @@ function getGroceryDataFromId(id){
     'name': groceryName,
     'items': groceryWithDepAndIng
   };
-  // return ""
 }
 
 
@@ -56,35 +38,29 @@ function getGroceryDataFromId(id){
 
 function getRandomRecipe(){
   return chickenKyiv.getRandomRecipe()
-  // return ""
 }
 
 function getFirstFiveRecipes(){
   return chickenKyiv.getFirstFiveRecipes();
-  // return ""
 }
 
 
 function getShowcaseFiveRecipes(){
   return chickenKyiv.getShowcaseFiveRecipes();
-  // return ""
 }
 
 
 function getFiveRandomIngredients(){
   return chickenKyiv.getFiveRandomIngredients();
-  // return ""
 }
 
 //@TODO i don't like this name too
 function getRecipeChickenKyivById(id) {
   return chickenKyiv.getRecipe()[id];
-  // return ""
 }
 
 function getRecipes() {
   return chickenKyiv.getRecipe();
-  // return ""
 }
 
 export {
